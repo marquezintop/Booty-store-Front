730d7fedcff84d7d3c0546cfdea8aa9179a591b7
unknown <marquez.cmbh@gmail.com>
2023-04-29 10:35:07 -0300

    feat: home page

---
+++ src/components/Header.js
@@ -0,0 +1,89 @@
+import { useNavigate } from "react-router-dom"
+import logo from "../assets/logo.png"
+import styled from "styled-components"
+
+export default function Header() {
+    const navigate = useNavigate()
+
+    return (
+        <HeaderStyled>
+            <img src={logo} alt="logo"/>
+            <div>
+                <button onClick={() => navigate("/")}>Home</button>
+                <button onClick={() => navigate("/about-us")}>About Us</button>
+                <button onClick={() => navigate("/partners")}>Partners</button>
+            </div>
+            <button className="sign-up">Sign-up now!</button>
+        </HeaderStyled>
+    )
+}
+
+const HeaderStyled = styled.div`
+width: 100vw;
+height: 140px; 
+background-color: #E4251B;
+position: fixed;
+top: 0;
+left: 0;
+display: flex;
+align-items: center;
+z-index: 2;
+img {
+    width: 224px;
+    margin-left: 70px;
+}
+div {
+    margin-left: 350px;
+    display: flex;
+    justify-content: space-between;
+    width: 500px;
+    button {
+        display: inline-block;
+        position: relative;
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 24px;
+        color: #FFFFFF;
+        border: none;
+        background-color: transparent;
+    }
+
+    button::after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        transform: scaleX(0);
+        height: 2px;
+        bottom: 0;
+        left: 0;
+        background-color: #FFFFFF;
+        transform-origin: bottom right;
+        transition: transform 0.25s ease-out;
+    }
+
+    button:hover::after {
+        transform: scaleX(1);
+        transform-origin: bottom left;
+    }
+}
+.sign-up {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  color: #FFFFFF;
+  width: 230px;
+  height: 68px;
+  background-color: #E4251B;
+  border: #FFFFFF 2px solid;
+  margin-left: 40px;
+  margin-right: 50px;
+  transition: all 0.3s ease-in-out;
+}
+
+.sign-up:hover {
+  background-color: #FFFFFF;
+  color: #E4251B;
+}
+`
---
+++ src/pages/HomePage.js
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+import lovers from "../assets/lovers.png"
+
+export default function HomePage() {
+    return (
+        <>
+        <Header></Header>
+        <StyledHome>
+            <StyledText>
+                <h1>Making and arranging dates just for you since 2023.</h1>
+                <p>Meet and choose your future dates to create an unforgettable experience.</p>
+            </StyledText>
+            <img src={lovers} alt="lovers"></img>
+            <StyledHomeGray>
+                <h1>Why use this site?</h1>
+                <p>
+                    The Booty Shop is the ideal choice for consumers who are seeking to find their perfect 
+                    romantic match. With a wide range of options and a user-friendly interface, the Booty Shop 
+                    offers an unparalleled online dating experience. Additionally, the Booty Store provides a 
+                    safe and secure platform for users to interact one another, you can even choice your 
+                    partner’s costume for the date. The Booty Shop is the perfect destination for anyone 
+                    looking to find their perfect match.
+                </p>
+            </StyledHomeGray>
+        </StyledHome>
+        </>
+    )
+}
+
+const StyledHome = styled.div`
+    width: 100vw;
+    height: 706px;
+    margin-top: 140px;
+    background-color: #E4251B;
+    display: flex;
+    img {
+        position: relative;
+        width: 460px;
+        height: 435px;
+        top: 100px;
+        left: 160px;
+        z-index: 1;
+    }
+`
+
+const StyledHomeGray = styled.div`
+    position: absolute;
+    top: 630px;
+    background-color: #DEDBD2;
+    width: 100vw;
+    height: 260px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    color: #E4251B;
+    h1 {
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 600;
+        font-size: 50px;
+        margin-top: 30px;
+    }
+    p {
+        width: 909px;
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        margin-top: 20px;
+    }
+`
+
+const StyledText = styled.div`
+    width: 585px;
+    display: flex;
+    flex-wrap: wrap;
+    color: #FFFFFF;
+    font-style: normal;
+    margin-left: 123px;
+    margin-top: 150px;
+    h1 {
+        font-family: 'Poppins';
+        font-weight: 600;
+        font-size: 40px;
+    }
+    p {
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        margin-top: -210px;
+    }
+`
