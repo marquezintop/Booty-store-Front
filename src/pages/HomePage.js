e837a4ee6eafb0118c000cb958649515c9e8f9bd
Guilfornari <guilfornari@gmail.com>
2023-05-01 16:10:14 -0300

    feat/meteors pages implemented

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,32 +1,31 @@
 import styled from "styled-components";
 import Header from "../components/Header";
 import meteor from "../assets/meteor.png"
-import { useEffect } from "react";
 
 export default function HomePage() {
 
     return (
         <>
-        <Header page={"Home"}/>
-        <StyledHome>
-            <StyledText>
-                <h1>Selling meteors at your fingertips since 2023</h1>
-                <p>Get to know our most varied types of meteors and buy the one that most sparkles your eyes.</p>
-            </StyledText>
-            <img src={meteor} alt="meteor"></img>
-            <StyledHomeGray>
-                <h1>Why buy from our store?</h1>
-                <p>
-                If you're in the market for a meteor, the Meteor Store is the place to go. Not only do we offer 
-                a wide selection of meteors to choose from, but we also provide a seamless online shopping 
-                experience. Our meteors are sourced from reputable suppliers, ensuring that you receive a 
-                high-quality product that will meet your expectations. Additionally, we offer competitive 
-                pricing and fast shipping, so you can have your meteor in your hands in no time. With our 
-                commitment to customer satisfaction, you can trust that your purchase from the Meteor Store will 
-                be a great investment for years to come.
-                </p>
-            </StyledHomeGray>
-        </StyledHome>
+            <Header page={"Home"} />
+            <StyledHome>
+                <StyledText>
+                    <h1>Selling meteors at your fingertips since 2023</h1>
+                    <p>Get to know our most varied types of meteors and buy the one that most sparkles your eyes.</p>
+                </StyledText>
+                <img src={meteor} alt="meteor"></img>
+                <StyledHomeGray>
+                    <h1>Why buy from our store?</h1>
+                    <p>
+                        If you're in the market for a meteor, the Meteor Store is the place to go. Not only do we offer
+                        a wide selection of meteors to choose from, but we also provide a seamless online shopping
+                        experience. Our meteors are sourced from reputable suppliers, ensuring that you receive a
+                        high-quality product that will meet your expectations. Additionally, we offer competitive
+                        pricing and fast shipping, so you can have your meteor in your hands in no time. With our
+                        commitment to customer satisfaction, you can trust that your purchase from the Meteor Store will
+                        be a great investment for years to come.
+                    </p>
+                </StyledHomeGray>
+            </StyledHome>
         </>
     );
 }
---
+++ src/pages/MeteorsPage.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+import MeteoriteCard from "../components/MeteoriteCard";
+import { useContext, useEffect, useState } from "react";
+import UserContext from "../contexts/UserContexts";
+import axios from "axios";
+
+
+export default function MeteorsPage() {
+    const { user } = useContext(UserContext);
+    const config = { headers: { Authorization: `Bearer ${user.token}` } };
+    const [meteoritesList, setMeteoritesList] = useState([]);
+
+    useEffect(() => {
+        getMeteoritesList();
+    }, []);
+
+
+    async function getMeteoritesList() {
+        try {
+            const res = await axios.get(`${process.env.REACT_APP_API_URL}/get-meteorites`, config);
+            setMeteoritesList(res.data);
+
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    };
+
+
+    return (
+        <>
+            <Header page={"Meteors"} />
+            <StyledMeteorsPage>
+                <StyledMeteorsContainer>
+                    {meteoritesList.map((meteorite) => <MeteoriteCard key={meteorite._id} meteorite={meteorite} />)}
+                </StyledMeteorsContainer>
+            </StyledMeteorsPage>
+
+        </>
+    );
+}
+
+const StyledMeteorsPage = styled.div`
+    width: 100vw;
+    max-width:100%;
+    height: 100vh;
+    margin-top: 140px;
+    background-color: #1D1F2D;
+    display: flex;
+`;
+
+const StyledMeteorsContainer = styled.div`
+    width: 90vw;
+    padding: 50px 250px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+`;
